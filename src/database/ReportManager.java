d7d6d41a5f91979b22ab1be6a525fe9e98914cc9
pat-aoc <patrick.ocampo@mail.utoronto.ca>
2019-07-21 23:10:13 -0400

    Made quite a few changes lol

--- src/database/ReportManager.java
+++ src/database/ReportManager.java
@@ -1,6 +1,19 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class ReportManager {
 
-    public
+    public static boolean totalNumberOfBookings(Connection conn, Scanner reader) {
+        try {
+            Statement st = conn.createStatement();
+        } catch (SQLException e) {
+            System.err.println("[ERROR] : Unable to get the report");
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
